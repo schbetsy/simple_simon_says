1d74169c67acb5e146f2fdab1caf9f9c8f9ded90
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 11:15:42 -0500

    wrote ajax post request, made json response

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,4 +9,10 @@ post '/color' do
 
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+
+  if request.xhr?
+    content_type(:json)
+    {cell: cell, color: color}.to_json
+  end
+
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
 
+function add_color(){
+  console.log("clicked");
+  $.post('/color',function (response) {
+    console.log(response);
+  });
+}
+
+$(document).ready(function(){
+  $('#get_color').on('click', add_color);
 });
